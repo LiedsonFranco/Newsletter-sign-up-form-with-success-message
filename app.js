4520b79988e3117f2e9674d40450e537261cc04c
Liedson Franco <126687225+LiedsonFranco@users.noreply.github.com>
2024-01-25 22:36:29 +0100

    css and html seted

---
+++ app.js
@@ -0,0 +1,16 @@
+function validarEmail(email) {
+    const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regexEmail.test(email);
+}
+function check_email(){
+    const email = document.getElementById('email').value
+    console.log('Super')
+    console.log(email)
+    if(validarEmail(email)){
+        document.querySelector('main').style.display = 'none'
+        console.log('Is valid')
+    }
+    else{
+        document.querySelector('.input-container').setAttribute('class', 'invalid-input')
+    }
+}
